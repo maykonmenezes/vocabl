bc42e31b4e8a9915a4c0d2317ce0dd2da41c5eee
Maykon Menezes <maykon.meneses@gmail.com>
2020-03-08 16:23:03 -0300

    Adding basic spec for words controller

--- spec/requests/words_request_spec.rb
+++ spec/requests/words_request_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Words", type: :request do
-
+  describe 'GET words' do
+    context 'When some words are present' do
+      it 'request list of all words' do
+        word = Word.create(value: 'dog')
+        get words_path
+        expect(response).to be_successful
+        expect(response.body).to include('dog')
+      end
+    end
+    
+    context 'When no words' do
+      it 'request list of all words' do
+        get words_path
+        expect(response).to be_successful
+        expect(response.body).to include('')
+      end
+    end
+  end
 end
