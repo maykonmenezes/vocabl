1d7623071d36291cc27abdc14f2634f13103122a
Maykon Menezes <maykon.meneses@gmail.com>
2020-03-08 20:46:06 -0300

    Add create and new methods

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,4 +2,21 @@ class WordsController < ApplicationController
   def index
     @words = Word.all
   end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(words_params)
+    if @word.save
+      redirect_to(words_path)
+    else
+      render :new
+    end
+  end
+
+  def word_params
+    params.require(:word).permit(:value, :language)
+  end
 end
