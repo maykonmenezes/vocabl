0307b6537c6315241fd33d3e2669f3f1de1d9864
Maykon Menezes <maykon.meneses@gmail.com>
2020-03-08 14:45:49 -0300

    Changing db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_08_153610) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "words", force: :cascade do |t|
     t.string "value"
     t.string "language"
