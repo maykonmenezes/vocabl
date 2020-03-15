40a7dd3fb1d3f5c07eaacbd3d78e720858afbf8f
Maykon Menezes <maykon.meneses@gmail.com>
2020-03-15 15:07:51 -0300

    Add answers_controller and spec

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root  'words#index'
   resources :words, only: %i[index new create show edit update destroy]
   resources :games, only: %i[new create show]
+  resources :answers, only: %i[create]
 end
