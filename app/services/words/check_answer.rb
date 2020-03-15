c59ac631fa129dfc15b0d124d010026500a8f4f0
Maykon Menezes <maykon.meneses@gmail.com>
2020-03-15 12:26:57 -0300

    Add service to check answer

---
+++ app/services/words/check_answer.rb
@@ -0,0 +1,19 @@
+module Words
+  class CheckAnswer
+    def initialize(word, answer)
+      @word = word
+      @answer = answer
+    end
+    def call
+      check_answer
+    end
+
+    private
+
+    attr_reader :word, :answer
+
+    def check_answer
+      word.translations.where(:content: answer).exists?
+    end
+  end
+end
