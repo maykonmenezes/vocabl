101b1f174cab25ab6c28425f2a908de15ac1ed98
Maykon Menezes <maykon.meneses@gmail.com>
2020-03-15 01:02:08 -0300

    Hide part of email address from the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_many :words
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def secret_email
+    email.gsub(/.{0,4}@/, '####@')
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe User, type: :model do
   describe 'associations' do
     it { is_expected.to have_many(:words) }
   end
+
+  describe 'secret_email' do
+    subject { user.secret_email }
+    let(:user) { build_stubbed(:user, email: 'peter.test@example.com') }
+
+    it { is_expected.to eq('peter.####@example.com') }
+  end
 end
