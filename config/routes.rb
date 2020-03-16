d7c946810c29cc97ddfc8282aee8363233fad70d
Maykon Menezes <maykon.meneses@gmail.com>
2020-03-15 23:52:15 -0300

    Add new fonts and controller to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'words#index'
+  resources :pages, only: :index
   resources :words, only: %i[index new create show edit update destroy]
   resources :games, only: %i[new create show]
   resources :answers, only: %i[create]
